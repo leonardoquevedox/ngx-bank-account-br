78a61e80c6fb1f96db58813610a239631ee8cf8a
Leonardo Quevedo <pac.leo@hotmail.com>
2019-02-16 00:52:36 -0200

    refact(bundle): Improves the way the module is bundled.

--- dist/lib/ngx-bank-account-br.component.d.ts
+++ dist/lib/ngx-bank-account-br.component.d.ts
@@ -1,3 +1,9 @@
+/**
+ * @license MIT
+ * @version 1.1.0
+ * @author Leonardo Quevedo
+ * @description Bank account card component.
+ */
 import { NgForm } from '@angular/forms';
 export declare class NgxBankAccountBrComponent {
     account: any;
--- dist/lib/ngx-bank-account-br.module.d.ts
+++ dist/lib/ngx-bank-account-br.module.d.ts
@@ -1,3 +1,8 @@
+/**
+ * @license MIT
+ * @author Leonardo Quevedo
+ * @description Directive module.
+ */
 import { NgxBankAccountBrComponent } from './ngx-bank-account-br.component';
 export declare const Directive: (typeof NgxBankAccountBrComponent)[];
 export declare class NgxBankAccountBrModule {
