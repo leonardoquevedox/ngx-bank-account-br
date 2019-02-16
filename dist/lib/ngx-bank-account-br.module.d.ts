796cea3bf527e0fdee821cfd8374ffbf00b2478b
Leonardo Quevedo <pac.leo@hotmail.com>
2019-02-15 23:21:25 -0200

    refact(bundle): Improves the way the module is bundled.

--- dist/lib/ngx-bank-account-br.module.d.ts
+++ dist/lib/ngx-bank-account-br.module.d.ts
@@ -1,8 +1,3 @@
-/**
- * @license MIT
- * @author Leonardo Quevedo
- * @description Directive module.
- */
 import { NgxBankAccountBrComponent } from './ngx-bank-account-br.component';
 export declare const Directive: (typeof NgxBankAccountBrComponent)[];
 export declare class NgxBankAccountBrModule {
